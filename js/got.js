8490b4e595c5e17d88b9cb6c842c7e69c203d0a6
Anita Adam <anita.aadam@gmail.com>
2018-12-08 21:28:54 +0100

    search not working, removed

--- js/got.js
+++ js/got.js
@@ -1,6 +1,6 @@
 function getGameOfThronesCharacterDatas(url, callbackFunc) {
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
       callbackFunc(this);
     }
@@ -17,24 +17,8 @@ function successGetGameOfThronesCharacterDatas(xhttp) {
   putArrayInOrder(living);
   charactersPortrait(living);
 
-  var portraits = document.querySelectorAll(".main_portraits");
-  for (var i = 0; i < portraits.length; i++) {
-    portraits[i].addEventListener(
-      "click",
-      //wrapper fgv, ami ahhoz kell, hogy atadjuk az event parametert
-      function(event) {
-        //a livingChaaracters-t latni akarjuk a getDetails fgv-en belul, ezert at kell adni parameterkent
-        //szukseg van az eventre is, hogy tudjuk mire kattintottunk, ezert azt is at kell adni parameterkent, ehhez kellett a wrapper
-        getDetails(living, event.target.alt);
-      }
-    );
-  }
-  var usersearchbutton = document.querySelector("#usersearchbutton");
-  usersearchbutton.addEventListener("click", function() {
-    var inputdata = document.querySelector("#usersearch").value;
-    search(inputdata, living);
-  });
 }
+
 
 getGameOfThronesCharacterDatas(
   "./json/got.json",
@@ -44,7 +28,7 @@ getGameOfThronesCharacterDatas(
 // Live servert használd mindig!!!!!
 /* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
 function putArrayInOrder(userDatas) {
-  userDatas.sort(function(first, second) {
+  userDatas.sort(function (first, second) {
     if (first.name > second.name) {
       return 1;
     }
@@ -78,27 +62,4 @@ function charactersPortrait(aliveUserDatas) {
                   `;
   }
   portraitRowElement.innerHTML = portraitRow;
-}
-
-function getDetails(living, name) {
-  var detail = "";
-  for (var i = 0; i < living.length; i++) {
-    if (living[i].name.toLowerCase() === name.toLowerCase()) {
-      detail += `
-      <div class="main_portraits">
-      <img src="${living[i].picture}" alt="${living[i].name}">
-      <p>${living[i].name}</p>
-      <p>${living[i].bio}</p>
-      </div>
-      `;
-    } else {
-      detail = "Character not found";
-    }
-    document.querySelector("#detail").innerHTML = detail;
-  }
-}
-
-function search(inputdata, living) {
-  getDetails(living, inputdata);
-  console.log(inputdata);
 }
